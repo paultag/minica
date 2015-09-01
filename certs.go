9bd4fff676c8412ad79501705ec0d3223851a039
Paul Tagliamonte <paultag@gmail.com>
2015-09-01 10:30:24 -0400

    Add in LICENSE headers.

--- certs.go
+++ certs.go
@@ -1,3 +1,19 @@
+/* {{{ Copyright 2015 Evan Hazlett (@ehazlett) <ejhazlett@gmail.com>
+ *     Copyright 2015 Aaron Huslage (@huslage) <huslage@gmail.com>
+ *     Copyright 2015 Paul Tagliamonte (@paultag) <paultag@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. }}} */
+
 package main
 
 import (
@@ -147,3 +163,5 @@ func GenerateCert(hosts []string, certFile, keyFile, caFile, caKeyFile, org, nam
 
 	return nil
 }
+
+// vim: foldmethod=marker
